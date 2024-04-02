6f4d406014b17e2b7536d2ecbbbd60370be4a995
sri harsha <109609898+codahal@users.noreply.github.com>
2024-04-02 14:31:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          H@rsh@ trigger jenkins build
+          H@rsh@ trigger 
         </p>
         <a
           className="App-link"
