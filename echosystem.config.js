8aa9b015637e24cc8418da99f6689d5baa7c635f
sri harsha <109609898+codahal@users.noreply.github.com>
2024-06-18 17:34:18 +0530

    Update echosystem.config.js

--- echosystem.config.js
+++ echosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    apps: [
-      {
-        name: "project_test",
-        script: "npm",
-        args: "start",
-        watch: true,
-        ignore_watch: ["node_modules"],
-        instances: 1,
-        autorestart: true,
-        max_memory_restart: "1G",
-        env: {
-          NODE_ENV: "production",
-        },
+  apps: [
+    {
+      name: 'project_test',
+      script: 'app.js', // Replace with the entry point of your application
+      instances: 'max', // Or a number of instances you want to run
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
