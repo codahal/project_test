f391151507d96a3f8f70093ec2bca0a5629e92c7
sri harsha <109609898+codahal@users.noreply.github.com>
2024-06-20 14:27:49 +0530

    Update echosystem.config.js

--- echosystem.config.js
+++ echosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'project_f',  // Change this to your application name
+      name: 'project_test',  // Change this to your application name
       script: 'npm',
       args: 'start',
       instances: 'max',
