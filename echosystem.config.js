426d278a1e7257b90fdb52fe73c414335644a3a1
sri harsha <109609898+codahal@users.noreply.github.com>
2024-06-20 14:26:22 +0530

    Update echosystem.config.js

--- echosystem.config.js
+++ echosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'project_test',
-      script: 'app.js', // Adjust the script name/path as necessary
+      name: 'project_f',  // Change this to your application name
+      script: 'npm',
+      args: 'start',
+      instances: 'max',
+      exec_mode: 'cluster',
       env: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
       }
-    },
-    // Other app definitions if any
+    }
   ]
 };
