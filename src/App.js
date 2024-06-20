fc0565bfc75b48c0adee708b5df2d43b3ccca69c
sri harsha <109609898+codahal@users.noreply.github.com>
2024-06-20 20:31:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          JENKINS Harsha 1
+          JENKINS Harsha 2
         </a>
       </header>
     </div>
