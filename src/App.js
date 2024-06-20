a3167a1c8528e94290f60c2a66e7842b9fbec91a
sri harsha <109609898+codahal@users.noreply.github.com>
2024-06-20 19:31:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          JENKINS Harsha 3
+          JENKINS Harsha trigger
         </a>
       </header>
     </div>
